4d1b21e05009c9b86bee19130638410201a2e01c
Mike <mike@blokur.com>
2022-05-02 09:24:39 +0100

    Add docs for client and server

--- internal/pkg/client/docs.go
+++ internal/pkg/client/docs.go
@@ -1,3 +1,25 @@
 // Package client implements the client side of the RISP protocol.
-// TODO: add more docs
+//
+// The client performs the following steps:
+//	1. Connect to the server.
+//	2. Send the initial handshake message with state CONNECTING, specifying the client UUID and sequence length and a small window size.
+// 	3. Receive the server response with the state CONNECTED, containing the first payload item.
+// 	4. The client repeatedly receives payloads and stores them at the correct place in the sequence.
+//  5. When the window size is exhausted, the client sends a CONNECTED message to the server acknowledging the payloads received in the window.
+// 	6. When all messages have been received, the client sends a CLOSING message to the server
+// 	7. The client receives the CLOSING reply from the server with the expected checksum.
+// 	8. The client sends the CLOSED message to the server, and receives the CLOSED reply.
+//  9. The client finally logs the result to stdout and disconnects from the server.
+//
+// An instance of Client captures the known state of the sequence in memory.
+//
+// The client also implements a dynamic window sizing strategy, whereby the window size
+// doubles when all values in the window are received without a disconnection by the client,
+// but is reset to a default small value on disconnection. This strategy therefore tries to
+// optimise the data exchange according to the connection stability.
+//
+// When the client disconnects, it returns ErrClientDisconnected. The reconnection must be performed by the caller.
+//
+// Additional flags can be specified to control the client message sending interval and the killswitch interval (to trigger disconnections).
+//
 package client
--- internal/pkg/server/docs.go
+++ internal/pkg/server/docs.go
@@ -1,3 +1,23 @@
 // Package server implements the server side of the RISP protocol.
-// TODO write docs
+//
+// The server performs the following steps:
+// 	1. Sets up a gRPC server to handle incoming connections from clients.
+// 	2. On connection with a client, it receives the initial handshake message with state CONNECTING,
+// 	   specifying the client UUID and sequence length and a small window size.
+// 	3. The server initialises the client session state with a random sequence, before sending the
+// 	   server response with the state CONNECTED, containing the first payload item.
+// 	4. The server repeatedly sends sequence items until the window size is exhausted.
+// 	5. When the server receives an acknowledgment from the client, it updates the session state for the client
+// 	   according to the new known state and continues to send the next payload items.
+// 	6. When the server receives a CLOSING message from the client, it sends the CLOSING reply with the expected checksum.
+// 	7. The server receives the CLOSED message from the client, and it sends the CLOSED reply.
+//
+// An instance of Server captures the expected state of the client in memory. When the client confirms the state,
+// it updates the client state in a session store. This session store could be adapted to a persistent store like Redis
+// to allow multiple server instances to handle client reconnections.
+//
+// Additional flags can be specified to control the server message sending interval.
+//
+// TODO: it would be nice to switch up message ordering, to demonstrate how the protocol can deal with this.
+// TODO: it would be nice to intermittently drop messages, to demonstrate how the protocol can deal with this.
 package server
